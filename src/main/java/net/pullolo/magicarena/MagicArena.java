738a0738c5ff287ba2c10ef040d4a304e8e259ab
BuildTools <unconfigured@null.spigotmc.org>
2023-09-12 18:22:59 +0200

    WorldManager added

--- src/main/java/net/pullolo/magicarena/MagicArena.java
+++ src/main/java/net/pullolo/magicarena/MagicArena.java
@@ -1,17 +1,22 @@
 package net.pullolo.magicarena;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import static net.pullolo.magicarena.worlds.WorldManager.copyWorld;
+import static net.pullolo.magicarena.worlds.WorldManager.removeWorld;
 
 public final class MagicArena extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        copyWorld(Bukkit.getWorld("world"), "copy");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        removeWorld(Bukkit.getWorld("copy"));
     }
 }
